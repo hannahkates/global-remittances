faa0e8c9a7dbba301ffa84b32e1f7f9e6a757fc6
Hannah Kates <hannah.b.kates@gmail.com>
2020-09-04 09:26:02 -0400

    add narrative, link to data source and github

--- js.js
+++ js.js
@@ -11,7 +11,7 @@ var formatNumber = d3.format(",.0f"),    // zero decimal places
     color = d3.scaleOrdinal(d3.schemeCategory20);
 
 // append the svg object to the body of the page
-var svg = d3.select("body").append("svg")
+var svg = d3.select("#chart").append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
   .append("g")
