321438a01cde9b4f334bf5f0692fdee2b3d429a4
Hannah Kates <hannah.b.kates@gmail.com>
2020-09-04 08:46:59 -0400

    annotate py script; update readme

--- README.md
+++ README.md
@@ -10,16 +10,20 @@ https://www.worldbank.org/en/topic/labormarkets/brief/migration-and-remittances
 ## Data preparation
 The data published by the World Bank is stored in an excel spreadsheet in an adjacency matrix format. The d3 network chart must consume a JSON file formatted to a specific standard, listing all the "nodes" and then the "links".
 
-#### Explanation of necessary JSON data structure:
+**Explanation of necessary JSON data structure:**
 https://www.d3-graph-gallery.com/graph/network_data_format.html
 
-#### Data munging steps:
-- Download excel spreadsheet and open in excel.
+**Data munging steps:**
+- Download excel spreadsheet from World Bank website and open in excel.
 - Remove extra columns and rows leaving only the matrix of individual countries.
-- Save as a CSV file. See Bilateralremittancematrix2018Oct2019.csv
-- Run python script to reformat the adjacency matrix CSV into a JSON format.
+- Save as a CSV file. See [Bilateralremittancematrix2018Oct2019.csv](https://github.com/hannahkates/global-remittances/blob/master/data/Bilateralremittancematrix2018Oct2019.csv)
+- Run [convert-adj-matrix-to-json.py](https://github.com/hannahkates/global-remittances/blob/master/data/convert-adj-matrix-to-json.py) python script to reformat the adjacency matrix CSV into the required JSON format.
+  - :warning: NOTE: This python script also filters which records to include based on the size of the transfer. The minimum is currently set to $3000 million, yielding ~30 country nodes. The app timed out when more nodes were included.
+- App uses [data.json](https://github.com/hannahkates/global-remittances/blob/master/data/data.json) outputted by the python script.
 
-## How to run this application
+## How to run this application locally
+- Clone repo `git clone https://github.com/hannahkates/global-remittances.git`
+- Run using python dev server `python -m SimpleHTTPServer`
 
 ## Resources
 This build was guided by https://bl.ocks.org/d3noob/013054e8d7807dff76247b81b0e29030
--- data/convert-adj-matrix-to-json.py
+++ data/convert-adj-matrix-to-json.py
@@ -4,44 +4,61 @@ import json
 with open('Bilateralremittancematrix2018Oct2019.csv', newline='') as csvfile:
     mydata = list(csv.reader(csvfile))
 
+# function that consumes the adjacency matrix from the CSV and
+# outputs the final json structure for d3 chart containing nodes and links
 def convert(adjacency_list):
-    # skip the index of 0, because in the CSV, row 0 and column 0 are used for the country names
-
-    # create empty list called links that will be populated during the for loop
-    links = []
 
     # specify minumum transfer $ amount to filter links by
     min_transfer = 3000
 
+    # create empty list called links that will store all the filtered links
+    links = []
+
+    # identify all links that have values over the minimum transfer $ amount
     for i,adj in enumerate(adjacency_list):
         # skip row 0 since it contains country names, not data
         if(i > 0):
-            # iterate through each item in the row, skipping column 0
+            # iterate through each item in the row, skipping column because it contains the country name
             for n in range(1, len(adj)):
-                # two conditions for recording links:
+                # three conditions for recording links:
                 # - avoid circular reference. a country shouldn't send remittances to itself
-                # - only keep links where remittances > 0
+                # - skips links where value is "N/A"
+                # - filter for links where value is more than minumum transfer $ amount
                 if (i != n) & (adj[n] != 'N/A'):
                     if float(adj[n]) > min_transfer:
-                        # store the source and target indices, renumbering with index 1 as 0
+                        # store the source and target country names and tranfer value
                         links.extend( [{"source":adj[0],"target":mydata[0][n],"value":float(adj[n])}] )
 
+    # create an empty array for storing all the nodes present in the filtered links
     used_nodes = []
+
+    # append all the source and target nodes into the used_nodes list
     for i in links:
         used_nodes.append(i["source"])
         used_nodes.append(i["target"])
 
+    # create sorted list of unique nodes
     used_nodes = sorted(list(set(used_nodes)))
 
+    # create an empty list for storing the nodes for the final json object
     nodes = []
+
+    # for loop prepares nodes and links for the final json object
+    # nodes: it stores the node index in addition to the name
+    # link: it updates the links object by replacing the node country names with indices
     for i in range(len(used_nodes)):
         nodes.extend( [{"index":i, "name":used_nodes[i]}] )
         for l in links:
+            # if source country name == country name
             if (l["source"] == used_nodes[i]):
+                # overwrite country name with county's node index
                 l["source"] = i
+            # if target country name == country name
             if (l["target"] == used_nodes[i]):
+                # overwrite country name with county's node index
                 l["target"] = i
 
+    # final json structure for d3 chart
     return {"nodes":nodes, "links":links}
 
 convert(mydata)
